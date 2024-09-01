d4acd5f4b303b2e71a7c2dec320bd810955a487f
auhulu <auhulu0@gmail.com>
2024-09-01 22:10:49 +0900

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,6 +12,11 @@ export default function App({ Component, pageProps }: any) {
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
         />
+        <meta property="og:title" content="MEGA BIG シミュレーター" />
+        <meta property="og:description" content="確変した第1476回 MEGA BIG のシミュレーターです" />
+        <meta property="og:image" content={"https://megabig.nwnwn.com/ogp.PNG"} />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary_large_image" />
         <link rel="shortcut icon" href="/favicon.svg" />
       </Head>
       <Component {...pageProps} />

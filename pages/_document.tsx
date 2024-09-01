37a238b160f55fad90738ce92c98a26b7d9149e0
auhulu <auhulu0@gmail.com>
2024-09-01 23:00:14 +0900

    update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,10 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta property="og:title" content="MEGA BIG シミュレーター" />
+        <meta property="og:description" content="確変した第1476回 MEGA BIG のシミュレーターです" />
+        <meta property="og:image" content={"https://megabig.nwnwn.com/ogp.PNG"} />
+        <meta name="twitter:card" content="summary_large_image" />
         <ColorSchemeScript />
       </Head>
       <body>

25205845fa7cbca38b9569bb771ef9be02da2ce1
auhulu <auhulu0@gmail.com>
2024-09-01 23:07:45 +0900

    update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,11 +3,13 @@ import { ColorSchemeScript } from "@mantine/core";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="ja">
       <Head>
         <meta property="og:title" content="MEGA BIG シミュレーター" />
         <meta property="og:description" content="確変した第1476回 MEGA BIG のシミュレーターです" />
-        <meta property="og:image" content={"https://megabig.nwnwn.com/ogp.PNG"} />
+        <meta property="og:image" content="https://megabig.nwnwn.com/ogp.PNG" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://megabig.nwnwn.com/" />
         <meta name="twitter:card" content="summary_large_image" />
         <ColorSchemeScript />
       </Head>
